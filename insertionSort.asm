77c8f9584d3b23609e7a6200c74b6d60a512e294
Alisson <alissonavlis96@gmail.com>
2018-10-30 17:26:14 -0300

    Insertion Sort e Selection Sort atuais

--- insertionSort.asm
+++ insertionSort.asm
@@ -1,5 +1,5 @@
 .data
-	vector: .word 32,45,6,76,12
+	Vetor: .word 2, 102, 41, 52, 3134, 2
 	primeiraparte: .asciiz "Indice: "
 	segundaparte: .asciiz " que possui valor: "
 	terceiraparte: .asciiz " troca com indice: "
@@ -8,67 +8,76 @@
 	space: .asciiz " "
 	
 .text
-	la $a0, vector
-	addi $a1, $zero, 5
-	addi $v0, $zero, 1
-	jal selection
-	addi $a1, $zero, 5
-	j printArray
-
+	la $a0, Vetor
+	addi $a1, $zero, 6
+	jal insertion
+	j printVetor
+	j exit
 	
-selection:#selection(int vector[], int size)
+insertion:
+	#a1 = tamanho e a0 = endereco do inicio do vetor
+	addi $sp,$sp,-8
+	sw $a1,0($sp)
+	sw $ra,4($sp)
+	
+	ble $a1,1,return
+	
+	addi $a1,$a1,-1
+	jal insertion
+	# $t1 = enderenço de memoria de vetor[tamanho - 1]
+	sll $t1, $a1, 2
+	add $t1, $a0, $t1
+	# $t1 = ultimo
+	lw $t1, 0($t1)
+	# j = tamanho - 2
+	addi $t2, $a1, -1
+	# $t3 = j + 4 (Indice do J)
+	sll $t3, $t2, 2
+	# $t3 = vetor[j]
+	add $t3, $a0, $t3
+	lw $t3, 0($t3)
+loop:
+	blt $t2, $zero, exitloop
+	ble $t3, $t1, exitloop
+	
+	#Funcao para printar a troca
+	#alocando pilha
 	addi $sp, $sp, -8
+	#salvando tamanho do vetor e endereco de ra
 	sw $a1, 0($sp)
 	sw $ra, 4($sp)
-	
-	#addi $a1, $a1 -1
-	#add $s3, $s3, 0
-	beq $s3, $a1, return
-	sll $s1, $s3, 2 #s1 = index_zero*4
-	add $s1, $a0, $s1 #s1 = v[0]
-
-	#lw $s6, 0($s1) #s6 = min_value	
-	#######FOR##########	
-	addi $s0, $s0, 1 #j=min+1 ###t0 = s1###
-	#add $t0, $s0, $zero
-
-forSel:
-
-	lw $s6, 0($s1) #s6 = min_value	
-	bge $t0, $a1, exitFor
-	sll $t5, $t0, 2 #t5 = index_j*4
-	add $t5, $a0, $t5 #t5 = v[j]
-	lw $s7, 0($t5)	#s7 = j_value
-	slt $t3, $t0, $a1 #if(j<=size) t3==1
-	beq $t3, $zero, exitFor
-	#blt $s7, $s6, if #if vector[j] < vector[min]
-	addi $t0, $t0, 1 #j++
-	blt $s7, $s6, if #if vector[j] < vector[min]
-
-	j forSel
-
-	
-exitFor: 
-	bge $s3, $a1, return #if s3>=size, return
-	addi $s3, $s3, 1
-	addi $s0, $s0, 1
-	jal selection
-
-return:
+	#a1 = j + 1
+	addi $a1, $t2, 1
+	#a2 = j
+	add $a2, $t2, $zero
+	#a3 = ultimo
+	add $a3, $t1, $zero 
+	jal printTroca
+	#restaurando a1 e ra
 	lw $a1, 0($sp)
 	lw $ra, 4($sp)
+	#desalocando
 	addi $sp, $sp, 8
+	#Continuacao do while
+	addi $t4, $t2, 1
+	sll $t4, $t4, 2
+	add $t4, $a0, $t4
+	sw $t3, 0($t4)
+	addi $t2, $t2, -1
+	sll $t3, $t2, 2
+	add $t3, $a0, $t3
+	lw $t3, 0($t3)
+	j loop
+exitloop:
+	addi $t4, $t2, 1
+	sll $t4, $t4, 2
+	add $t4, $a0, $t4
+	sw $t1, 0($t4)
+return:
+	lw $a1,0($sp)
+	lw $ra,4($sp)
+	addi $sp,$sp,8
 	jr $ra
-if:	
-	########SWAP#########
-	add $t9, $s6, $zero #temp = min
-	add $s6, $s7, $zero #min = j
-	sw $s6, 0($s1) 
-	sw $t9, 0($t5)
-	addi $s3, $s3, 1
-	#####################
-	j selection
-	
 printTroca:
 	addi $sp, $sp, -4
 	sw $a0, 0($sp)
@@ -105,13 +114,12 @@ printTroca:
 	syscall
 	lw $a0, 0($sp)
 	addi $sp, $sp, 4
-	jr $ra	
-	
-printArray:
+	jr $ra
+printVetor:
 	add $s0, $zero, $zero
 	add $a2, $a0, $zero
-	for:slt $t9, $s0, $a1
-	beq $t9, $zero, exit
+	for:slt $t0, $s0, $a1
+	beq $t0, $zero, exit
 	sll $t1, $s0, 2
 	add $t1, $a2, $t1
 	lw $a0, 0($t1)
--- selectionSort.asm
+++ selectionSort.asm
@@ -1,89 +1,132 @@
 .data
-	vector: .word 5, 16, 2, 67, 23
+	vector: .word 32,45,6,76,12
+	primeiraparte: .asciiz "Indice: "
+	segundaparte: .asciiz " que possui valor: "
+	terceiraparte: .asciiz " troca com indice: "
+	quartaparte: .asciiz " que possui valor: "
+	quebralinha: .asciiz "\n"
+	space: .asciiz " "
 	
 .text
 	la $a0, vector
-	addi $a1, $zero, 0
-	addi $a2, $zero, 4
-	jal swap
-	add $a0, $v0, $zero
+	addi $a1, $zero, 5
 	addi $v0, $zero, 1
-	syscall
-	add $a0, $v1, $zero
-	syscall
-	j exit
+	jal selection
+	addi $a1, $zero, 5
+	j printArray
 	
-swap:#swap(int vector[], int index1, int index2)
-	sll $t1, $a1, 2 #t1 = index_a1*4
-	sll $t3, $a2, 2 #t3 = index_a2*4
-	add $t1, $a0, $t1 #t1 = v[a1]
-	lw $t0, 0($t1)
-	add $t1, $a0, $t3 #t1 = v[a2]
-	lw $t2, 0($t1)
-	
-	sw $t2, 0($t1)
-	#sw $t0, 0($t1)
-	
-	add $v0, $t0, $zero
-	add $v1, $t2, $zero
-	jr $ra
-	
-minIndex:#minIndex(int vector[], int index, int size)
-	addi $sp, $sp, -16
-	sw $ra, 12($sp)
-	sw $a2, 8($sp)
-	sw $a1, 4($sp)
-	sw $a0, 0($sp)
-	
-	
-	beq $a1, $a2, returnI
-	sll $t1, $a1, 2 #t1 = index*4
-	add $t1, $a0, $t1 #t1 = vector[index]
-	lw $t2, 0($t1)
-	addi $a1, $a1, 1 #index = index + 1
-	add $t0, $a1, $zero #t0 = index+1
-	sll $t0, $a1, 2 #t0 = index*4
-	add $t1, $a0, $t0 #t1 = vector[index+1]
-	lw $t3, 0($t1)
-	
-	blt $t2, $t3, returnIndex
-	blt $t3, $t2, returnIndex2
 
-	jal minIndex
-	
-returnI:#In case index==size, return index
-	add $v0, $a1, $zero
-	lw $a0, 0($sp)
-	lw $a1, 4($sp)
-	lw $a2, 8($sp)
-	lw $ra, 12($sp)
-	addi $sp, $sp, 16
-	
-	jr $ra
-	
-returnIndex:#If vector[index] is smaller than vector[index+1], execute this label
-	add $v0, $t2, $zero
-	lw $a0, 0($sp)
-	lw $a1, 4($sp)
-	lw $a2, 8($sp)
-	lw $ra, 12($sp)
-	addi $sp, $sp, 16
-	
-	jr $ra
-
-returnIndex2:#If vector[index+1] is smaller than vector[index], execute this label
-	add $v0, $t3, $zero
-	lw $a0, 0($sp)
-	lw $a1, 4($sp)
-	lw $a2, 8($sp)
-	lw $ra, 12($sp)
-	addi $sp, $sp, 16
-	
-	jr $ra
 
 	
 selection:#selection(int vector[], int size)
-	add $t0, $zero, $zero #index = 0
-	beq $a1, $t0, exitSelection
-		
+	addi $sp, $sp, -8
+	sw $a1, 0($sp)
+	sw $ra, 4($sp)
+
+	beq $s3, $a1, return
+	sll $s1, $s3, 2 #s1 = index_zero*4
+	add $s1, $a0, $s1 #s1 = v[0]
+	
+	add $t0, $t0, $zero
+
+forSel:
+
+	lw $s6, 0($s1) #s6 = min_value	
+	sll $t5, $t0, 2 #t5 = index_j*4
+	add $t5, $a0, $t5 #t5 = v[j]
+	lw $s7, 0($t5)	#s7 = j_value
+	slt $t3, $t0, $a1 #if(j<=size) t3==1
+	beq $t3, $zero, exitFor
+	addi $t0, $t0, 1 #j++
+	blt $s7, $s6, if #if vector[j] < vector[min]
+	bge $t0, $a1, exitFor
+
+	j forSel
+
+	
+exitFor: 
+	bge $s3, $a1, return #if s3>=size, return
+	addi $s3, $s3, 1
+	add $t0, $s3, 0
+
+	jal selection
+
+return:
+	lw $a1, 0($sp)
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
+	jr $ra
+if:	
+
+	add $t9, $s6, $zero #temp = min
+	add $s6, $s7, $zero #min = j
+	addi $t0, $t0, 1
+
+swap:
+	########SWAP#########
+	sw $s6, 0($s1) 
+	sw $t9, 0($t5)
+	addi $s3, $s3, 1
+	addi $s0, $s0, 1
+	#####################
+	j selection
+	
+printTroca:
+	addi $sp, $sp, -4
+	sw $a0, 0($sp)
+	la $a0, primeiraparte
+	addi $v0, $zero, 4
+	syscall
+	add $a0, $a1, $zero
+	addi $v0, $zero, 1
+	syscall
+	la $a0, segundaparte
+	addi $v0, $zero, 4
+	syscall
+	add $a0, $a3, $zero
+	addi $v0, $zero, 1
+	syscall
+	la $a0, terceiraparte
+	addi $v0, $zero, 4
+	syscall
+	add $a0, $a2, $zero
+	addi $v0, $zero, 1
+	syscall
+	la $a0, quartaparte
+	addi $v0, $zero, 4
+	syscall
+	lw $a0, 0($sp)
+	sll $t8, $a2, 2
+	add $t8, $t8, $a0
+	lw $t8, 0($t8)
+	add $a0, $t8, $zero
+	addi $v0, $zero, 1
+	syscall
+	la $a0, quebralinha
+	addi $v0, $zero, 4
+	syscall
+	lw $a0, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra	
+	
+
+	
+printArray:
+	add $s0, $zero, $zero
+	add $a2, $a0, $zero
+	for:slt $t9, $s0, $a1
+	beq $t9, $zero, exit
+	sll $t1, $s0, 2
+	add $t1, $a2, $t1
+	lw $a0, 0($t1)
+	addi $v0, $zero, 1
+	syscall
+	addi $s0, $s0, 1
+	la $a0, space
+	addi $v0, $zero, 4
+	syscall
+	j for
+	lw $a0, 0($t1)
+	
+
 exit: nop
